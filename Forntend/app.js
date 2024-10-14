4d4d5abeca59b27f819ccab474e4ae4a505e5494
Pradeep S Belavigi <pradeepsbelavigi26@gmail.com>
2024-10-14 21:01:07 +0530

    add talk function

--- Forntend/app.js
+++ Forntend/app.js
@@ -3,5 +3,10 @@ function Person(name,age){
     this.age = age;
 };
 
+Person.prototype.talk = function(){
+    console.log("Hello, my name is " + this.name + " and age is " + this.age);
+}
+
+
 let person1=new Person("Anu",21);
 let person2=new Person("Deepu",22);
