fd4611978c927069c0bd5b401850096dbcd4efb2
Pradeep S Belavigi <pradeepsbelavigi26@gmail.com>
2024-10-14 21:25:14 +0530

    add classes

--- Forntend/app.js
+++ Forntend/app.js
@@ -10,3 +10,16 @@ Person.prototype.talk = function(){
 
 let person1=new Person("Anu",21);
 let person2=new Person("Deepu",22);
+
+class mens{
+    constructor(name,age){
+        this.name=name;
+        this.age=age;
+    }
+    talk(){
+        console.log(`Hi, my name is ${this.name} and my age is ${this.age}`);
+    }
+}
+
+let man1=new mens("Ananya",21);
+let man2=new mens("Sakshi",22); 
