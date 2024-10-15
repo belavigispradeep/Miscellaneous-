d4b96aca0380b504c8bdf180922989ebc0dd5b86
Pradeep S Belavigi <pradeepsbelavigi26@gmail.com>
2024-10-15 08:28:09 +0530

    add the inheritance

--- Forntend/app.js
+++ Forntend/app.js
@@ -23,3 +23,27 @@ class mens{
 
 let man1=new mens("Ananya",21);
 let man2=new mens("Sakshi",22); 
+
+class Animals{
+    constructor(name,type){
+        this.name=name;
+        this.type=type;
+    }
+    attack(){
+        console.log(`${this.name} is Danger Animal`);
+    }
+}
+
+class Lion extends Animals{
+    constructor(name,type,food){
+        super(name,type);
+        this.food=food;
+        }
+}
+
+class Parrot extends Animals{
+    constructor(name,type,food){
+        super(name,type);
+        this.food=food;
+        }
+}
